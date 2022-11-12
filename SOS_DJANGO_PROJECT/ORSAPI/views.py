b091398844976b8538d76a606f67c8784bfd6cf5
SnorlaxRays <sandeepratnawat1106@gmail.com>
2022-11-12 13:35:42 +0530

    success

--- SOS_DJANGO_PROJECT/ORSAPI/views.py
+++ SOS_DJANGO_PROJECT/ORSAPI/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from .restctl.LoginCtl import LoginCtl
+from .restctl.RoleCtl import RoleCtl
+from .restctl.UserCtl import UserCtl
+from .restctl.RegistrationCtl import RegistrationCtl
+from .restctl.CourseCtl import CourseCtl
+from .restctl.CollegeCtl import CollegeCtl
+from .restctl.StudentCtl import StudentCtl
+from .restctl.FacultyCtl import FacultyCtl
+from .restctl.TimeTableCtl import TimeTableCtl
+from .restctl.SubjectCtl import SubjectCtl
+from .restctl.ChangepasswordCtl import ChangepasswordCtl
+from .restctl.ForgetpasswordCtl import ForgetpasswordCtl
+from .restctl.MarksheetCtl import MarksheetCtl
 
-# Create your views here.
+# Create your view here
+def info(request,page,action):
+    print("Request Method -----",request.method)
+    print("Page -----",page)
+    print("Action -----", action)
+    print("Base Path:---- ", __file__)
+
+
+@csrf_exempt
+def action(request,page,action="get",id=0,pageNo=1):
+    print("ID ---",id)
+    info(request,page,action)
+    methodCall = page + "Ctl()." + action + "(request,{'id':id, 'pageNo':pageNo})"
+    response = eval(methodCall)
+    return response
+
